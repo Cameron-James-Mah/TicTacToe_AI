e30631c40ddb19de1bd5f797c1fd9e2a736f2baf
Cameron Mah <emilsdespair@gmail.com>
2023-06-06 04:52:57 -0400

    Some refactoring, style changes

--- public/main.js
+++ public/main.js
@@ -103,8 +103,8 @@ function checkBoard(){
     // Checking of Player X finish
     // Checking for Player 0 starts, Is player 0 won or
     // not and after that disabled all the other fields
-    else if ((b1 == '0' || b1 == '0') && (b2 == '0' || 
-        b2 == '0') && (b3 == '0' || b3 == '0')) {
+    else if ((b1 == 'O' || b1 == 'O') && (b2 == 'O' || 
+        b2 == 'O') && (b3 == 'O' || b3 == 'O')) {
         document.getElementById("b4").disabled = true;
         document.getElementById("b5").disabled = true;
         document.getElementById("b6").disabled = true;
@@ -113,8 +113,8 @@ function checkBoard(){
         document.getElementById("b9").disabled = true;
         document.getElementById('print').innerHTML = "AI Won"
     }
-    else if ((b1 == '0' || b1 == '0') && (b4 == '0' || 
-        b4 == '0') && (b7 == '0' || b7 == '0')) {
+    else if ((b1 == 'O' || b1 == 'O') && (b4 == 'O' || 
+        b4 == 'O') && (b7 == 'O' || b7 == 'O')) {
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b5").disabled = true;
@@ -123,8 +123,8 @@ function checkBoard(){
         document.getElementById("b9").disabled = true;
         document.getElementById('print').innerHTML = "AI Won"
     }
-    else if ((b7 == '0' || b7 == '0') && (b8 == '0' || 
-        b8 == '0') && (b9 == '0' || b9 == '0')) {
+    else if ((b7 == 'O' || b7 == 'O') && (b8 == 'O' || 
+        b8 == 'O') && (b9 == 'O' || b9 == 'O')) {
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
@@ -133,8 +133,8 @@ function checkBoard(){
         document.getElementById("b6").disabled = true;
         document.getElementById('print').innerHTML = "AI Won"
     }
-    else if ((b3 == '0' || b3 == '0') && (b6 == '0' || 
-        b6 == '0') && (b9 == '0' || b9 == '0')) {
+    else if ((b3 == 'O' || b3 == 'O') && (b6 == 'O' || 
+        b6 == 'O') && (b9 == 'O' || b9 == 'O')) {
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -143,8 +143,8 @@ function checkBoard(){
         document.getElementById("b8").disabled = true;
         document.getElementById('print').innerHTML = "AI Won"
     }
-    else if ((b1 == '0' || b1 == '0') && (b5 == '0' || 
-        b5 == '0') && (b9 == '0' || b9 == '0')) {
+    else if ((b1 == 'O' || b1 == 'O') && (b5 == 'O' || 
+        b5 == 'O') && (b9 == 'O' || b9 == 'O')) {
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -153,8 +153,8 @@ function checkBoard(){
         document.getElementById("b8").disabled = true;
         document.getElementById('print').innerHTML = "AI Won"
     }
-    else if ((b3 == '0' || b3 == '0') && (b5 == '0' || 
-        b5 == '0') && (b7 == '0' || b7 == '0')) {
+    else if ((b3 == 'O' || b3 == 'O') && (b5 == 'O' || 
+        b5 == 'O') && (b7 == 'O' || b7 == 'O')) {
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -163,8 +163,8 @@ function checkBoard(){
         document.getElementById("b9").disabled = true;
         document.getElementById('print').innerHTML = "AI Won"
     }
-    else if ((b2 == '0' || b2 == '0') && (b5 == '0' || 
-        b5 == '0') && (b8 == '0' || b8 == '0')) {
+    else if ((b2 == 'O' || b2 == 'O') && (b5 == 'O' || 
+        b5 == 'O') && (b8 == 'O' || b8 == 'O')) {
         document.getElementById("b1").disabled = true;
         document.getElementById("b3").disabled = true;
         document.getElementById("b4").disabled = true;
@@ -173,8 +173,8 @@ function checkBoard(){
         document.getElementById("b9").disabled = true;
         document.getElementById('print').innerHTML = "AI Won"
     }
-    else if ((b4 == '0' || b4 == '0') && (b5 == '0' || 
-        b5 == '0') && (b6 == '0' || b6 == '0')) {
+    else if ((b4 == 'O' || b4 == 'O') && (b5 == 'O' || 
+        b5 == 'O') && (b6 == 'O' || b6 == 'O')) {
         document.getElementById("b1").disabled = true;
         document.getElementById("b2").disabled = true;
         document.getElementById("b3").disabled = true;
@@ -186,12 +186,12 @@ function checkBoard(){
   
     // Checking of Player 0 finish
     // Here, Checking about Tie
-    else if ((b1 == 'X' || b1 == '0') && (b2 == 'X' 
-        || b2 == '0') && (b3 == 'X' || b3 == '0') && 
-        (b4 == 'X' || b4 == '0') && (b5 == 'X' || 
-        b5 == '0') && (b6 == 'X' || b6 == '0') && 
-        (b7 == 'X' || b7 == '0') && (b8 == 'X' || 
-        b8 == '0') && (b9 == 'X' || b9 == '0')) {
+    else if ((b1 == 'X' || b1 == 'O') && (b2 == 'X' 
+        || b2 == 'O') && (b3 == 'X' || b3 == 'O') && 
+        (b4 == 'X' || b4 == 'O') && (b5 == 'X' || 
+        b5 == 'O') && (b6 == 'X' || b6 == 'O') && 
+        (b7 == 'X' || b7 == 'O') && (b8 == 'X' || 
+        b8 == 'O') && (b9 == 'X' || b9 == 'O')) {
         document.getElementById('print').innerHTML = "Tie"
     }
 }
@@ -209,7 +209,7 @@ function start() {
     if(moves){
         for (let move of moves){//do a minimax call for each possible strating move
             let newBoard = copyBoard(board)
-            newBoard[move.y][move.x] = '0'
+            newBoard[move.y][move.x] = 'O'
             let currVal = minimax(newBoard, false)
             if(currVal > bestVal){
                 best = move
@@ -218,41 +218,41 @@ function start() {
         }
         console.log(bestVal)
         //console.log(best)
-        board[best.y][best.x] = '0'
+        board[best.y][best.x] = 'O'
         if(best.y == 0 && best.x == 0){
-            document.getElementById("b1").value = "0";
+            document.getElementById("b1").value = 'O';
             document.getElementById("b1").disabled = true;
         }
         else if(best.y == 0 && best.x == 1){
-            document.getElementById("b2").value = "0";
+            document.getElementById("b2").value = 'O';
             document.getElementById("b2").disabled = true;
         }
         else if(best.y == 0 && best.x == 2){
-            document.getElementById("b3").value = "0";
+            document.getElementById("b3").value = 'O';
             document.getElementById("b3").disabled = true;
         }
         else if(best.y == 1 && best.x == 0){
-            document.getElementById("b4").value = "0";
+            document.getElementById("b4").value = 'O';
             document.getElementById("b4").disabled = true;
         }
         else if(best.y == 1 && best.x == 1){
-            document.getElementById("b5").value = "0";
+            document.getElementById("b5").value = 'O';
             document.getElementById("b5").disabled = true;
         }
         else if(best.y == 1 && best.x == 2){
-            document.getElementById("b6").value = "0";
+            document.getElementById("b6").value = 'O';
             document.getElementById("b6").disabled = true;
         }
         else if(best.y == 2 && best.x == 0){
-            document.getElementById("b7").value = "0";
+            document.getElementById("b7").value = 'O';
             document.getElementById("b7").disabled = true;
         }
         else if(best.y == 2 && best.x == 1){
-            document.getElementById("b8").value = "0";
+            document.getElementById("b8").value = 'O';
             document.getElementById("b8").disabled = true;
         }
         else if(best.y == 2 && best.x == 2){
-            document.getElementById("b9").value = "0";
+            document.getElementById("b9").value = 'O';
             document.getElementById("b9").disabled = true;
         }
         else{
@@ -268,7 +268,7 @@ function minimax(currentBoard, maximizing){
   //get valid moves, if no valid moves then reached depth
   let curr = 0
   //console.log(currentBoard)
-  if(winningMove(currentBoard, '0') && !maximizing){
+  if(winningMove(currentBoard, 'O') && !maximizing){
     return 1
   }
   if(winningMove(currentBoard, 'X') && maximizing){
@@ -281,17 +281,17 @@ function minimax(currentBoard, maximizing){
     }
     /*
     for(let move of moves){//check for winning moves
-        if(winningMove(currentBoard, '0')){ 
+        if(winningMove(currentBoard, 'O')){ 
             return 1
         }
     }*/
-    if(winningMove(currentBoard, '0')){
+    if(winningMove(currentBoard, 'O')){
         return 1
     }
     curr = -1
     for(let move of moves){
         let newBoard = copyBoard(currentBoard)
-        newBoard[move.y][move.x] = '0'    
+        newBoard[move.y][move.x] = 'O'    
         curr = Math.max(minimax(newBoard, false), curr)
     }
     
